153f101111543d018fa3be7b7480c4c8795fa64d
amy <ctrlalternativedelete@gmail.com>
2022-07-21 22:07:14 +1000

    added deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd build
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/jy1263/rotmg-skin-viewer.git master:gh-pages
+
+cd -
